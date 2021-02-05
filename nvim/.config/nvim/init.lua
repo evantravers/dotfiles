a8272e0ca4ec9f7ed21932a5d0f6f964193b7000
Evan Travers <evantravers@gmail.com>
2021-02-05 11:54:31 -0600

    Configure sumneko for lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -265,6 +265,35 @@ require'lspconfig'.solargraph.setup({
   }
 })
 
+local sumneko_root_path = "/users/evantravers/.config/lsp/lua-language-server"
+local sumneko_binary = sumneko_root_path.."/bin/macOS/lua-language-server"
+
+require'lspconfig'.sumneko_lua.setup {
+  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = vim.split(package.path, ';'),
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        },
+      },
+    },
+  },
+}
+
 paq 'nvim-lua/completion-nvim'
 paq 'nvim-lua/lsp_extensions.nvim'
 
