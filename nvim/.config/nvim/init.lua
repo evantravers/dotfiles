c844d1d6c6d7c852c37fc4e892370346b26d7c40
Evan Travers <evantravers@gmail.com>
2021-02-06 20:02:10 -0600

    Recognize globals

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -280,7 +280,7 @@ require'lspconfig'.sumneko_lua.setup {
       },
       diagnostics = {
         -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
+        globals = {'vim', 'hs'},
       },
       workspace = {
         -- Make the server aware of Neovim runtime files
