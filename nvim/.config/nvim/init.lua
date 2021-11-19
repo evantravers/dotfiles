f988653cb17945a75693c37046c2b1c08013e383
Evan Travers <evantravers@gmail.com>
2021-11-19 15:08:12 -0600

    Update find command
    Found on reddit, borrowed from:
    
    https://github.com/skbolton/titan/blob/4d0d31cc6439a7565523b1018bec54e3e8bc502c/nvim/nvim/lua/mappings/filesystem.lua#L6

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -108,7 +108,7 @@ function _G.searchWiki()
   }
 end
 
-vim.api.nvim_set_keymap('n', '<c-p>', ":lua require('telescope.builtin').find_files()<cr>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<c-p>', ":lua require('telescope.builtin').find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<localleader><space>', ":lua require('telescope.builtin').buffers()<cr>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<localleader>ww', ":lua _G.searchWiki()<cr>", {noremap = true, silent = true})
 
