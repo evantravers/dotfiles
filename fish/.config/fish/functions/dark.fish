cd424fae0c3476bbd0ca96570c32323455c01188
Evan Travers <evantravers@gmail.com>
2021-11-19 13:33:54 -0600

    Add dark and light themes

---
+++ fish/.config/fish/functions/dark.fish
@@ -0,0 +1,3 @@
+function dark -d "Set dark theme"
+  kitty @ set-colors -a "~/.local/share/nvim/site/pack/paqs/start/zenbones.nvim/extras/kitty/zenbones_dark.conf"
+end
---
+++ fish/.config/fish/functions/light.fish
@@ -0,0 +1,3 @@
+function light -d "Set light theme"
+  kitty @ set-colors -a "~/.local/share/nvim/site/pack/paqs/start/zenbones.nvim/extras/kitty/zenbones_light.conf"
+end
