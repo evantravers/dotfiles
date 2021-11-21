c6c299b236c20c62833fd5e1be79bdae44ca11cb
Evan Travers <evantravers@gmail.com>
2021-11-21 08:29:56 -0600

    Update colors for pager and theme

--- fish/.config/fish/functions/colors.fish
+++ fish/.config/fish/functions/colors.fish
@@ -4,7 +4,7 @@ function colors -d "Set theme"
   set -U fish_color_normal white
 
   # commands like echo
-  set -U fish_color_command brgreen -o
+  set -U fish_color_command brwhite -o
 
   # keywords like if - this falls back on command color if unset
   set -U fish_color_keyword purple
@@ -37,38 +37,23 @@ function colors -d "Set theme"
   set -U fish_color_escape yellow
 
   # autosuggestions (the proposed rest of a command)
-  set -U fish_color_autosuggestion brblack
-
-  # the current working directory in the default prompt
-  set -U fish_color_cwd
-
-  # the username in the default prompt
-  set -U fish_color_user
-
-  # the hostname in the default prompt
-  set -U fish_color_host
-
-  # the hostname in the default prompt for remote sessions (like ssh)
-  set -U fish_color_host_remote
-
-  # the '^C' indicator on a canceled command
-  set -U fish_color_cancel red
+  set -U fish_color_autosuggestion brblack -di
 
   # history search matches and selected pager items (background only)
   set -U fish_color_search_match orange
 
   # PAGER
   # the progress bar at the bottom left corner
-  set -U fish_pager_color_progress green
+  set -U fish_pager_color_progress blue -d
 
   # the prefix string, i.e. the string that is to be completed
   set -U fish_pager_color_prefix
 
   # the completion itself, i.e. the proposed rest of the string
-  set -U fish_pager_color_completion magenta
+  set -U fish_pager_color_completion normal -i
 
   # the completion description
-  set -U fish_pager_color_description yellow
+  set -U fish_pager_color_description normal -d
 
   # background of the selected completion
   set -U fish_pager_color_selected_background bryellow
@@ -81,8 +66,4 @@ function colors -d "Set theme"
 
   # description of the selected completion
   set -U fish_pager_color_selected_description brblack
-
-  set -U pure_color_mute cyan
-  set -U pure_color_primary brblack
-  set -U pure_color_success green
 end
