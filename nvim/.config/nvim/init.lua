0050fbe44b7899175b22fb55599c08c6fc651d85
Evan Travers <evantravers@gmail.com>
2021-12-02 12:14:25 -0600

    Enable "typewriter scrolling"
    https://randomdeterminism.wordpress.com/2011/08/15/typewriter-scroll-mode-in-vim/

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -414,11 +414,13 @@ function _G.toggleProse()
     },
     on_open = function()
       if (vim.bo.filetype == "markdown") then
+        vim.cmd 'set so=999'
         vim.cmd 'packadd vim-pencil'
         vim.cmd 'PencilSoft'
       end
     end,
     on_close = function()
+      vim.cmd 'set so=3'
       if (vim.bo.filetype == "markdown") then
         vim.cmd 'PencilOff'
       end
