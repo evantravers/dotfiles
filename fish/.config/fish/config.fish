e34acae888febba00c3be843f20e4a8c4f6172d5
Evan Travers <evantravers@gmail.com>
2022-01-10 12:56:22 -0600

    Auto-launch tmux on new kitty sessions
    Thank you @tmiller
    
    https://github.com/tmiller/fae/commit/882c124e60551662f3da25714d0d42235a48f4d1

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,1 +1,13 @@
 starship init fish | source
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Launch tmux immediately after loading the shell
+if command --query tmux
+  and status is-interactive
+  and not string match --quiet --regex 'screen|tmux' $TERM
+  and test -z $TMUX
+  exec tmux new-session -A -s main
+end
