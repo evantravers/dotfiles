82cbb18b0ca6af1c1ddb9db70c55f316f64f8b97
Evan Travers <evantravers@gmail.com>
2022-02-21 09:53:29 -0600

    Remove unused hook

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,5 @@
 starship init fish | source
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 # Launch tmux immediately after loading the shell
 if command --query tmux
   and status is-interactive
