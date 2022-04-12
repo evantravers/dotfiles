e8ef2e5902de89b078b7b299b66cef76c194fd9c
Evan Travers <evantravers@gmail.com>
2022-04-11 21:17:59 -0500

    Use vim.keymap.set

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -152,14 +152,14 @@ local zk = require 'zk-nvim'
 zk.setup()
 
 
-vim.api.nvim_set_keymap('n', '<c-p>', ":lua require('telescope.builtin').find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<localleader><space>', ":lua require('telescope.builtin').buffers()<cr>", {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<localleader>ww', ":lua _G.searchWiki()<cr>", {noremap = true, silent = true})
+vim.keymap.set('n', '<c-p>', ":lua require('telescope.builtin').find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", {noremap = true, silent = true})
+vim.keymap.set('n', '<localleader><space>', ":lua require('telescope.builtin').buffers()<cr>", {noremap = true, silent = true})
+vim.keymap.set('n', '<localleader>ww', ":lua _G.searchWiki()<cr>", {noremap = true, silent = true})
 
 
 -- easyalign
-vim.api.nvim_set_keymap('v', '<Enter>', '<Plug>(EasyAlign)', {})
-vim.api.nvim_set_keymap('n', '<Leader>a', '<Plug>(EasyAlign)', {})
+vim.keymap.set('v', '<Enter>', '<Plug>(EasyAlign)', {})
+vim.keymap.set('n', '<Leader>a', '<Plug>(EasyAlign)', {})
 
 
 -- vim-sandwich
@@ -382,57 +382,57 @@ require'nvim-treesitter.configs'.setup {
 local saga = require('lspsaga')
 saga.init_lsp_saga()
 -- code finder
-vim.api.nvim_set_keymap('n',
+vim.keymap.set('n',
   'gh',
   "<cmd>lua require'lspsaga.provider'.lsp_finder()<CR>",
   {noremap = true, silent = true})
 -- docs
-vim.api.nvim_set_keymap('n',
+vim.keymap.set('n',
   'K',
   "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>",
   {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n',
+vim.keymap.set('n',
   '<C-f>',
   "<cmd>lua require('lspsaga.hover').smart_scroll_hover(1)<CR>",
   {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n',
+vim.keymap.set('n',
   '<C-b>',
   "<cmd>lua require('lspsaga.hover').smart_scroll_hover(-1)<CR>",
   {noremap = true, silent = true})
 -- code actions
-vim.api.nvim_set_keymap('n',
+vim.keymap.set('n',
   '<space>ca',
   "<cmd>lua require('lspsaga.codeaction').code_action()<CR>",
   {noremap = true, silent = true})
-vim.api.nvim_set_keymap('v',
+vim.keymap.set('v',
   '<space>ca',
   "<cmd>'<,'>lua require('lspsaga.codeaction').range_code_action()<CR>",
   {noremap = true, silent = true})
 -- signature help
-vim.api.nvim_set_keymap('n',
+vim.keymap.set('n',
   '<space>k',
   "<cmd>lua require('lspsaga.signaturehelp').signature_help()<CR>",
   {noremap = true, silent = true})
 -- rename
-vim.api.nvim_set_keymap('n',
+vim.keymap.set('n',
   '<space>rn',
   "<cmd>lua require('lspsaga.rename').rename()<CR>",
   {noremap = true, silent = true})
 -- preview definition
-vim.api.nvim_set_keymap('n',
+vim.keymap.set('n',
   '<space>gd',
   "<cmd>lua require'lspsaga.provider'.preview_definition()<CR>",
   {noremap = true, silent = true})
 -- diagnostics
-vim.api.nvim_set_keymap('n',
+vim.keymap.set('n',
   '<space>d',
   "<cmd>lua require'lspsaga.diagnostic'.show_line_diagnostics()<CR>",
   {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n',
+vim.keymap.set('n',
   '[d',
   "<cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_prev()<CR>",
   {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n',
+vim.keymap.set('n',
   ']d',
   "<cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_next()<CR>",
   {noremap = true, silent = true})
@@ -478,7 +478,7 @@ end
 vim.g['pencil#conceallevel'] = 0
 vim.g['pencil#wrapModeDefault'] = 'soft'
 
-vim.api.nvim_set_keymap(
+vim.keymap.set(
   'n',
   '<localleader>m',
   ':lua _G.toggleProse()<cr>',
@@ -487,12 +487,12 @@ vim.api.nvim_set_keymap(
 
 -- Covenience macros
 -- "..." -> "…"
-vim.api.nvim_set_keymap('n',
+vim.keymap.set('n',
   '<leader>fe',
   ":%s,\\.\\.\\.,…,g<CR>:nohlsearch<CR>",
   {noremap = true, silent = true})
 
-vim.api.nvim_set_keymap('n',
+vim.keymap.set('n',
   '<leader>a',
   "<cmd>lua require('telescope.builtin').live_grep()<cr>",
   {noremap = true, silent = true})
