238975c4be2227dae799a2a05b4401c0f0a4a891
Evan Travers <evantravers@gmail.com>
2023-02-03 20:37:15 -0600

    Update lspsaga config to new standard

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -477,63 +477,81 @@ require'lspconfig'.sumneko_lua.setup {
 }
 
 -- floating windows
-local saga = require('lspsaga')
-saga.init_lsp_saga()
--- code finder
-vim.keymap.set('n',
-  'gh',
-  "<cmd>lua require'lspsaga.provider'.lsp_finder()<CR>",
-  {noremap = true, silent = true})
--- docs
-vim.keymap.set('n',
-  'K',
-  "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>",
-  {noremap = true, silent = true})
-vim.keymap.set('n',
-  '<C-f>',
-  "<cmd>lua require('lspsaga.hover').smart_scroll_hover(1)<CR>",
-  {noremap = true, silent = true})
-vim.keymap.set('n',
-  '<C-b>',
-  "<cmd>lua require('lspsaga.hover').smart_scroll_hover(-1)<CR>",
-  {noremap = true, silent = true})
--- code actions
-vim.keymap.set('n',
-  '<space>ca',
-  "<cmd>lua require('lspsaga.codeaction').code_action()<CR>",
-  {noremap = true, silent = true})
-vim.keymap.set('v',
-  '<space>ca',
-  "<cmd>'<,'>lua require('lspsaga.codeaction').range_code_action()<CR>",
-  {noremap = true, silent = true})
--- signature help
-vim.keymap.set('n',
-  '<space>k',
-  "<cmd>lua require('lspsaga.signaturehelp').signature_help()<CR>",
-  {noremap = true, silent = true})
--- rename
-vim.keymap.set('n',
-  '<space>rn',
-  "<cmd>lua require('lspsaga.rename').rename()<CR>",
-  {noremap = true, silent = true})
--- preview definition
-vim.keymap.set('n',
-  '<space>gd',
-  "<cmd>lua require'lspsaga.provider'.preview_definition()<CR>",
-  {noremap = true, silent = true})
--- diagnostics
-vim.keymap.set('n',
-  '<space>d',
-  "<cmd>lua require'lspsaga.diagnostic'.show_line_diagnostics()<CR>",
-  {noremap = true, silent = true})
-vim.keymap.set('n',
-  '[d',
-  "<cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_prev()<CR>",
-  {noremap = true, silent = true})
-vim.keymap.set('n',
-  ']d',
-  "<cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_next()<CR>",
-  {noremap = true, silent = true})
+local saga = require('lspsaga').setup({})
+-- LSP finder - Find the symbol's definition
+-- If there is no definition, it will instead be hidden
+-- When you use an action in finder like "open vsplit",
+-- you can use <C-t> to jump back
+vim.keymap.set("n", "gh", "<cmd>Lspsaga lsp_finder<CR>")
+
+-- Code action
+vim.keymap.set({"n","v"}, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+
+-- Rename all occurrences of the hovered word for the entire file
+vim.keymap.set("n", "gr", "<cmd>Lspsaga rename<CR>")
+
+-- Rename all occurrences of the hovered word for the selected files
+vim.keymap.set("n", "gr", "<cmd>Lspsaga rename ++project<CR>")
+
+-- Peek definition
+-- You can edit the file containing the definition in the floating window
+-- It also supports open/vsplit/etc operations, do refer to "definition_action_keys"
+-- It also supports tagstack
+-- Use <C-t> to jump back
+vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>")
+
+-- Go to definition
+vim.keymap.set("n","gd", "<cmd>Lspsaga goto_definition<CR>")
+
+-- Show line diagnostics
+-- You can pass argument ++unfocus to
+-- unfocus the show_line_diagnostics floating window
+vim.keymap.set("n", "<leader>sl", "<cmd>Lspsaga show_line_diagnostics<CR>")
+
+-- Show cursor diagnostics
+-- Like show_line_diagnostics, it supports passing the ++unfocus argument
+vim.keymap.set("n", "<leader>sc", "<cmd>Lspsaga show_cursor_diagnostics<CR>")
+
+-- Show buffer diagnostics
+vim.keymap.set("n", "<leader>sb", "<cmd>Lspsaga show_buf_diagnostics<CR>")
+
+-- Diagnostic jump
+-- You can use <C-o> to jump back to your previous location
+vim.keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+vim.keymap.set("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
+
+-- Diagnostic jump with filters such as only jumping to an error
+vim.keymap.set("n", "[E", function()
+  require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
+end)
+vim.keymap.set("n", "]E", function()
+  require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
+end)
+
+-- Toggle outline
+vim.keymap.set("n","<leader>o", "<cmd>Lspsaga outline<CR>")
+
+-- Hover Doc
+-- If there is no hover doc,
+-- there will be a notification stating that
+-- there is no information available.
+-- To disable it just use ":Lspsaga hover_doc ++quiet"
+-- Pressing the key twice will enter the hover window
+vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>")
+
+-- If you want to keep the hover window in the top right hand corner,
+-- you can pass the ++keep argument
+-- Note that if you use hover with ++keep, pressing this key again will
+-- close the hover window. If you want to jump to the hover window
+-- you should use the wincmd command "<C-w>w"
+vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc ++keep<CR>")
+
+-- Call hierarchy
+vim.keymap.set("n", "<Leader>ci", "<cmd>Lspsaga incoming_calls<CR>")
+vim.keymap.set("n", "<Leader>co", "<cmd>Lspsaga outgoing_calls<CR>")
+
+-- Floating terminal
+vim.keymap.set({"n", "t"}, "<A-d>", "<cmd>Lspsaga term_toggle<CR>")
 
 
 -- PROSE MODE
