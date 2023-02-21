adea8f6378a7b5fc5b50d1073c294d9f51560761
Evan Travers <evantravers@gmail.com>
2023-02-20 22:02:06 -0600

    Remove plugins I don't use

--- fish/.config/fish/completions/fasd.fish
+++
@@ -1,1 +0,0 @@
-complete -c fasd -a "(__fasd_print_completion)" -f -A
--- fish/.config/fish/conf.d/__fasd_run.fish
+++
@@ -1,3 +0,0 @@
-function update_fasd_db --on-event fish_preexec -d "fasd takes record of the directories changed into"
-    command fasd --proc (command fasd --sanitize "$argv") > "/dev/null" 2>&1
-end
