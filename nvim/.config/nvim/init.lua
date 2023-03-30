bc4ac23d15e71f5d1ebce1aa688ebde067b887e9
Evan Travers <evantravers@gmail.com>
2023-03-30 13:47:43 -0500

    Move vim options and set background transparent

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -84,8 +84,11 @@ require("lazy").setup({
 })
 
 -- THEME
-vim.g.zenbones_solid_line_nr = true
-vim.g.zenbones_solid_vert_split = true
+vim.g.zenbones = {
+  solid_line_nr = true,
+  solid_vert_split = true,
+  transparent_background = true
+}
 vim.cmd [[color zenbones]]
 
 require'nvim-treesitter.configs'.setup {
