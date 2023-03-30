58e62c514c22d4587177169c7c2c935d36d08283
Evan Travers <evantravers@gmail.com>
2023-03-30 13:48:08 -0500

    Align by =

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -85,8 +85,8 @@ require("lazy").setup({
 
 -- THEME
 vim.g.zenbones = {
-  solid_line_nr = true,
-  solid_vert_split = true,
+  solid_line_nr          = true,
+  solid_vert_split       = true,
   transparent_background = true
 }
 vim.cmd [[color zenbones]]
