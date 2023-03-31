afc48ffdbba795ccc069ebefd73343f6f75198cb
Evan Travers <evantravers@gmail.com>
2023-03-30 22:05:20 -0500

    Remove unused plugins

--- fish/.config/fish/functions/dark.fish
+++
@@ -1,4 +0,0 @@
-function dark -d "Set dark theme"
-  set -xU theme "dark"
-  kitty @ set-colors -a "~/.local/share/nvim/lazy/zenbones.nvim/extras/kitty/zenbones_dark.conf"
-end
--- fish/.config/fish/functions/light.fish
+++
@@ -1,4 +0,0 @@
-function light -d "Set light theme"
-  set -xU theme "light"
-  kitty @ set-colors -a "~/.local/share/nvim/lazy/zenbones.nvim/extras/kitty/zenbones_light.conf"
-end
