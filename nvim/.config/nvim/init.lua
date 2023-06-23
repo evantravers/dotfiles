69e2ae7c132de059535b71921ed87e21414cb09e
Evan Travers <evantravers@gmail.com>
2023-06-23 16:24:54 -0500

    Try out flash
    So far... pretty neat.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -22,6 +22,39 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   -- UI
   "cormacrelf/dark-notify",  -- switch light/dark
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          -- default options: exact mode, multi window, all directions, with a backdrop
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+    },
+  },
   "junegunn/vim-easy-align",
   "kopischke/vim-fetch",     -- be able to open from stack traces
   "lewis6991/gitsigns.nvim", -- gitsigns
