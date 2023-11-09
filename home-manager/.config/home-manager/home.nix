24b7ab4d7f4da151b55e507a85f50afc19321362
Evan Travers <evantravers@gmail.com>
2023-11-09 15:59:53 +0000

    Add editor

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -65,7 +65,7 @@
   #
   # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    # EDITOR = "nvim";
   };
 
   # Let Home Manager install and manage itself.
