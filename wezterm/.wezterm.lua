87896b4d9eea4bc9124b5a69d34b79c8343e3b07
Evan Travers <evantravers@gmail.com>
2023-11-11 20:10:14 -0600

    Use numerical weight

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -12,7 +12,7 @@ return {
   font = wezterm.font_with_fallback {
     {
       family = 'Monaspace Neon Var',
-      weight = 'Medium',
+      weight = 600,
       harfbuzz_features = { "calt", "liga", "dlig", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" },
     },
   },
