29069a77ea50ce52ceb9b327f2cccdb8290da091
Evan Travers <evantravers@gmail.com>
2023-11-14 04:06:43 +0000

    Rewriting most of tmux into nix

--- home.nix
+++ home.nix
@@ -17,7 +17,6 @@
   # environment.
   home.packages = [
     pkgs.neovim
-    pkgs.tmux
     pkgs.git
     pkgs.ripgrep
   ];
@@ -31,9 +30,6 @@
     # nvim
     ".config/nvim/.vimrc".source = nvim/.config/nvim/.vimrc;
     ".config/nvim/init.lua".source = nvim/.config/nvim/init.lua;
-    # tmux
-    ".config/tmux/shared.conf".source = tmux/.config/tmux/shared.conf;
-    ".config/tmux/tmux.conf".source = tmux/.config/tmux/tmux.conf;
     # starship?
     ".config/starship/starship.toml".source = starship/.config/starship.toml;
   };
@@ -48,9 +44,100 @@
   # Use fish
   programs.fish = {
     enable = true;
+
+    interactiveShellInit = ''
+      set fish_greeting # N/A
+    '';
   };
 
   programs.starship = {
     enable = true;
+  };
+
+  programs.tmux = {
+    enable = true;
+    sensibleOnTop = false;
+    prefix = "C-space";
+    escapeTime = 0;
+    shell = "${pkgs.fish}/bin/fish";
+    terminal = "wezterm";
+
+    extraConfig = ''
+    # use vi keys in search mode
+    set-window-option -g mode-keys vi
+
+    # status bar
+    set-option -g status-left " #S "
+    set-option -g status-right "#[bold]%-l:%M ⋮ #[default italics]%a, %b %d #[default]⋮ #{battery_icon}"
+    set-option -g set-titles-string "tmux:#I #W"
+
+    # window status (tab selected/unselected)
+    set-window-option -g window-status-current-format "#[fg=colour7] ■ #I:#W#{?window_zoomed_flag, ,}"
+    set-window-option -g window-status-format " □ #I:#W"
+
+    # options from tmux-sensible
+
+    # address vim mode switching delay (http://superuser.com/a/252717/65504)
+    set -s escape-time 0
+
+    # increase scrollback buffer size
+    set -g history-limit 50000
+
+    # tmux messages are displayed for 4 seconds
+    set -g display-time 4000
+
+    # refresh 'status-left' and 'status-right' more often
+    set -g status-interval 5
+
+    # emacs key bindings in tmux command prompt (prefix + :) are better than
+    # vi keys, even for vim users
+    set -g status-keys emacs
+
+    # focus events enabled for terminals that support them
+    set -g focus-events on
+
+    # super useful when using "grouped sessions" and multi-monitor setup
+    setw -g aggressive-resize on
+
+    # more colors and themes
+    set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'  # undercurl support
+    set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'  # underscore colours - needs tmux-3.0
+    set -g message-style bg="colour0",fg="colour3"
+    set -g pane-active-border-style fg="colour8"
+    set -g pane-border-style fg="colour8"
+    set -g status-position "top"
+    set -g status-style bg="default",fg="colour15"
+    set-window-option -g window-status-current-style fg="colour15"
+
+    # dim inactive window text
+    set -g window-style fg=colour15
+    set -g window-active-style fg=colour7
+
+    # cool battery options
+    set -g @batt_icon_charge_tier9 "󰂂"
+    set -g @batt_icon_charge_tier8 "󰂁"
+    set -g @batt_icon_charge_tier7 "󰂀"
+    set -g @batt_icon_charge_tier6 "󰁿"
+    set -g @batt_icon_charge_tier5 "󰁾"
+    set -g @batt_icon_charge_tier4 "󰁽"
+    set -g @batt_icon_charge_tier3 "󰁼"
+    set -g @batt_icon_charge_tier2 "󰁻"
+    set -g @batt_icon_charge_tier1 "󰁺"
+    set -g @batt_icon_status_charged "󰁹"
+    set -g @batt_icon_status_charging "󰂄"
+    set -g @batt_icon_status_discharging "󱊥"
+    set -g @batt_icon_status_attached "ﮣ"
+    set -g @batt_color_status_primary_charged "#3daee9"
+    set -g @batt_color_status_primary_charging "#3daee9"
+    '';
+
+    plugins = with pkgs.tmuxPlugins; [
+      tmux-thumbs
+      battery
+      logging
+      pain-control
+      sessionist
+      yank
+    ];
   };
 }
