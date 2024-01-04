33fe5ac26b66e15695640213e6679886c7782889
Evan Travers <evantravers@gmail.com>
2024-01-04 06:37:50 -0600

    Change bind because I use this for splits

--- home.nix
+++ home.nix
@@ -410,7 +410,7 @@
               vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
               vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
               vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-              vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+              vim.keymap.set('n', '<M-k>', vim.lsp.buf.signature_help, opts)
               vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
               vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
               vim.keymap.set('n', '<space>wl', function()
