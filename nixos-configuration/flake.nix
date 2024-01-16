ed5948b9188bcbc6a45cd241c995eae7fcb4e900
Evan Travers <evantravers@gmail.com>
2024-01-15 22:45:17 -0600

    Starting to refactor into flakes

---
+++ nixos-configuration/flake.nix
@@ -0,0 +1,8 @@
+{
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
