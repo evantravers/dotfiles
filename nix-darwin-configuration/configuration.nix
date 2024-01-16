e57ddf472cd7e5183b024d1bc72dfb3af9895f68
Evan Travers <evantravers@gmail.com>
2024-01-16 07:51:27 -0600

    In theory, this should work.
    In practice, the previous commit does not.

--- nix-darwin-configuration/configuration.nix
+++ nix-darwin-configuration/configuration.nix
@@ -10,7 +10,7 @@
 
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+  environment.darwinConfig = "$HOME/src/github.com/evantravers/dotfiles/nix-darwin-configuration/flake.nix";
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
