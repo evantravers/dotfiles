674704a8373a560b30bf236ebb4a370184ef1b14
Evan Travers <evantravers@gmail.com>
2024-01-29 21:39:31 -0600

    Get nix-darwin into flakes

--- nix-darwin-configuration/flake.nix
+++ nix-darwin-configuration/flake.nix
@@ -2,7 +2,7 @@
   description = "Evan's darwin system";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -10,13 +10,6 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs }: {
     darwinConfigurations = {
       "G2157QVFX1" = nix-darwin.lib.darwinSystem {
-        # localSettings = {
-        #   home = {
-        #     username = "etravers";
-        #     homeDirectory = "/Users/etravers/";
-        #   };
-        #   hostPlatform = "aarch64-darwin";
-        # };
         modules = [ ./configuration.nix ];
         specialArgs = { inherit inputs; };
       };
