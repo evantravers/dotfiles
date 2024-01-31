e91d8f91e0f2cbd32a6774284a3346c5600b1d82
Evan Travers <evantravers@gmail.com>
2024-01-30 21:27:45 -0600

    Pin to stable

--- nixos-configuration/flake.nix
+++ nixos-configuration/flake.nix
@@ -1,8 +1,14 @@
 {
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+  };
+
   outputs = { self, nixpkgs }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+      modules = [
+        ./configuration.nix
+      ];
     };
   };
 }
