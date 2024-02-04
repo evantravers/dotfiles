e842c348c87a0ee4d4d56afebba31ba9ebce3421
Evan Travers <evantravers@gmail.com>
2024-02-04 14:23:46 -0600

    Pin home-manager too

--- nix-darwin-configuration/flake.nix
+++ nix-darwin-configuration/flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
