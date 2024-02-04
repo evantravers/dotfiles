831ed10112253d5ac36ee9f0078b6fb61c66a790
Evan Travers <evantravers@gmail.com>
2024-02-03 19:01:10 -0600

    Update url
    unsure if that's causing the mismatch, doesn't look like that's it.

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
     flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
