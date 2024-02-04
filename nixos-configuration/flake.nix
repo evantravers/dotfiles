b45a36ded26a1adc2447a5fae0e677d8f7de60d3
Evan Travers <evantravers@gmail.com>
2024-02-03 19:03:23 -0600

    pin home-manager to the version we are using for nix
    This removes the version mismatch warning.

--- nixos-configuration/flake.nix
+++ nixos-configuration/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
