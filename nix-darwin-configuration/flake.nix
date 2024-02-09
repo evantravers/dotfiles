c5e9a379e02a939fdf5a264d4e04c4e6cf9aa33b
Evan Travers <evantravers@gmail.com>
2024-02-08 20:47:18 -0600

    Use unstable for a bit
    Someday I'll be smart enough to overlay this, but this'll work for now.
    
    https://github.com/NixOS/nixpkgs/pull/270227

--- nix-darwin-configuration/flake.nix
+++ nix-darwin-configuration/flake.nix
@@ -2,8 +2,8 @@
   description = "Evan's darwin system";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
