f2ced7450a8074e13599b613b9fdc3df61d15d67
Evan Travers <evantravers@gmail.com>
2024-02-09 07:02:11 -0600

    Make nixos-wsl pure

--- nixos-configuration/flake.nix
+++ nixos-configuration/flake.nix
@@ -3,15 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ...}: {
+  outputs = inputs@{ nixpkgs, home-manager, nixos-wsl, ...}: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
+          nixos-wsl.nixosModules.wsl
           ./configuration.nix
           home-manager.nixosModules.home-manager
           {
