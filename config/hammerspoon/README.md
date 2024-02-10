4330769f13e268e2341f06460593a442b80fa584
Evan Travers <evantravers@gmail.com>
2024-02-09 20:53:01 -0600

    Moving stuff around for the final version
    I'm trying to have something a bit more flexible and hopefully runnable
    from the web, something like https://github.com/kclejeune/system

---
+++ config/hammerspoon/README.md
@@ -0,0 +1,45 @@
+# Personal Hammerspoon Setup
+
+Intended to live in `~/.hammerspoon`
+To install: `git clone git@github.com:evantravers/hammerspoon-config.git ~/.hammerspoon`
+
+## Requirements
+
+- Hyper requires Karabiner-elements, or some way of binding an F19 key (I bind
+  left control -> F19)
+
+## Modules
+
+### Autolayout
+
+- Listens to display changes and moves and maximizes windows based on screen
+  preferences.
+
+### Brave
+
+- Provides bindings for Brave Browser
+    - Focus tab by domain in any window
+    - Kill tabs by domain
+
+### Hyper
+
+Moved to [https://github.com/evantravers/Hyper.spoon](https://github.com/evantravers/Hyper.spoon)
+
+### Headspace
+
+Moved to [https://github.com/evantravers/headspace.spoon](https://github.com/evantravers/headspace.spoon)
+
+### Movewindows
+
+Moved to [https://github.com/evantravers/MoveWindows.spoon/](https://github.com/evantravers/MoveWindows.spoon/)
+
+One interesting binding for auto splitting an reference application, moved out to [https://github.com/evantravers/split.spoon](https://github.com/evantravers/split.spoon)
+
+### Secrets
+
+Simple loading of API keys or secret variables into `hs.settings` via
+`hs.json`.
+
+### Browsersnip
+
+Quickly snipping ZK-style notes from browsers into Drafts.app
---
+++ config/hammerspoon/configApplications.lua
@@ -0,0 +1,144 @@
+return {
+  ['com.raycast.macos'] = {
+    bundleID = 'com.raycast.macos',
+    localBindings = {'c', 'space', 'o'}
+  },
+  ['com.github.wez.wezterm'] = {
+    bundleID = 'com.github.wez.wezterm',
+    hyperKey = 'j',
+    tags = {'coding'}
+  },
+  ['com.brave.Browser'] = {
+    bundleID = 'com.brave.Browser',
+    tags = {'browsers'},
+  },
+  ['org.mozilla.firefox'] = {
+    bundleID = 'org.mozilla.firefox',
+    tags = {'browsers'}
+  },
+  ['com.apple.Safari'] = {
+    bundleID = 'com.apple.Safari',
+    tags = {'browsers'}
+  },
+  ['com.google.Chrome'] = {
+    bundleID = 'com.google.Chrome',
+    tags = {'browsers'}
+  },
+  ['com.kapeli.dashdoc'] = {
+    bundleID = 'com.kapeli.dashdoc',
+    hyperKey = 'h',
+    tags = {'coding'}
+  },
+  ['com.microsoft.teams2'] = {
+    bundleID = 'com.microsoft.teams2',
+    tags = {'communication', 'chat'}
+  },
+  ['com.apple.mail'] = {
+    bundleID = 'com.apple.mail',
+    hyperKey = 'e',
+    tags = {'communication', 'distraction'}
+  },
+  ['com.flexibits.fantastical2.mac'] = {
+    bundleID = 'com.flexibits.fantastical2.mac',
+    hyperKey = 'y',
+    localBindings = {'/'},
+    tags = {'planning', 'review', 'calendar'},
+    whitelisted = true,
+  },
+  ['com.apple.finder'] = {
+    bundleID = 'com.apple.finder',
+    hyperKey = 'f'
+  },
+  ['com.hnc.Discord'] = {
+    bundleID = 'com.hnc.Discord',
+    tags = {'distraction', 'chat'},
+  },
+  ['com.tinyspeck.slackmacgap'] = {
+    bundleID = 'com.tinyspeck.slackmacgap',
+    tags = {'distraction', 'communication', 'chat'},
+  },
+  ['com.tapbots.Tweetbot3Mac'] = {
+    bundleID = 'com.tapbots.Tweetbot3Mac',
+    tags = {'distraction', 'socialmedia'},
+  },
+  ['com.culturedcode.ThingsMac'] = {
+    bundleID = 'com.culturedcode.ThingsMac',
+    hyperKey = 't',
+    tags = {'planning', 'review', 'tasks'},
+    whitelisted = true,
+    localBindings = {',', '.'},
+  },
+  ['com.agiletortoise.Drafts-OSX'] = {
+    bundleID = 'com.agiletortoise.Drafts-OSX',
+    hyperKey ='d',
+    tags = {'review', 'writing', 'research', 'notes'},
+    whitelisted = true,
+    localBindings = {'x', ';'}
+  },
+  ['com.joehribar.toggl'] = {
+    bundleID = 'com.joehribar.toggl',
+    hyperKey = 'n'
+  },
+  ['com.ideasoncanvas.mindnode.macos'] = {
+    bundleID = 'com.ideasoncanvas.mindnode.macos',
+    tags = {'research'},
+  },
+  ['com.apple.MobileSMS'] = {
+    bundleID = 'com.apple.MobileSMS',
+    tags = {'communication', 'distraction', 'personal'},
+  },
+  ['com.valvesoftware.steam'] = {
+    bundleID = 'com.valvesoftware.steam',
+    tags = {'distraction'}
+  },
+  ['net.battle.app'] = {
+    bundleID = 'net.battle.app',
+    tags = {'distraction'}
+  },
+  ['com.spotify.client'] = {
+    bundleID = 'com.spotify.client'
+  },
+  ['com.figma.Desktop'] = {
+    bundleID = 'com.figma.Desktop',
+    tags = {'design'},
+  },
+  ['com.reederapp.5.macOS'] = {
+    bundleID = 'com.reederapp.5.macOS',
+    tags = {'distraction'},
+  },
+  ['md.obsidian'] = {
+    bundleID = 'md.obsidian',
+    hyperKey = 'g',
+    tags = {'research', 'notes'},
+  },
+  ['us.zoom.xos'] = {
+    bundleID = 'us.zoom.xos',
+  },
+  ['org.whispersystems.signal-desktop'] = {
+    bundleID = 'org.whispersystems.signal-desktop',
+    tags = {'distraction', 'communication', 'personal'}
+  },
+  ['ru.keepcoder.Telegram'] = {
+    bundleID = 'ru.keepcoder.Telegram',
+    tags = {'distraction', 'communication', 'personal'}
+  },
+  ['com.surteesstudios.Bartender'] = {
+    bundleID = 'com.surteesstudios.Bartender',
+    localBindings = {'b'}
+  },
+  ['com.loom.desktop'] = {
+    bundleID = 'com.loom.desktop',
+  },
+  ['company.thebrowser.Browser'] = {
+    bundleID = 'company.thebrowser.Browser',
+    tags = {'browsers'}
+  },
+  ['com.dexterleng.Homerow'] = {
+    bundleID = 'com.dexterleng.Homerow',
+    localBindings = {'l'}
+  },
+  ['com.flexibits.cardhop.mac'] = {
+    bundleID = 'com.flexibits.cardhop.mac',
+    localBindings = {'u'}
+  }
+}
