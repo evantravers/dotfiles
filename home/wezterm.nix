fceeb069a15dffd52fb67fd69cf5797dd48dd777
Evan Travers <evantravers@gmail.com>
2024-02-13 15:31:43 -0600

    Push wezterm stuff to a module

---
+++ home/wezterm.nix
@@ -0,0 +1,11 @@
+{lib, pkgs, ...}:
+{
+  home.file.".config/wezterm/wezterm.lua".source = ../config/wezterm/.wezterm.lua;
+
+  home.activation.installWeztermProfile = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    tempfile=$(mktemp) \
+    && ${pkgs.curl}/bin/curl -o $tempfile https://raw.githubusercontent.com/wez/wezterm/main/termwiz/data/wezterm.terminfo \
+    && tic -x -o ~/.terminfo $tempfile \
+    && rm $tempfile
+  '';
+}
