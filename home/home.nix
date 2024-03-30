a0d6d0157137d3e743649a4ba137c5ec3143765f
Evan Travers <evantravers@gmail.com>
2024-03-30 09:14:09 -0500

    Add devenv

--- home/home.nix
+++ home/home.nix
@@ -14,6 +14,7 @@
     # The home.packages option allows you to install Nix packages into your
     # environment.
     packages = [
+      pkgs.devenv
       pkgs.ltex-ls
       pkgs.marksman
       pkgs.nixd
