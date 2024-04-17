68b560d6c4d68deb92c3b0129779e3df33926fbb
Evan Travers <evantravers@gmail.com>
2024-04-17 11:03:22 -0500

    Adjusting the wezterm config

--- config/wezterm/.wezterm.lua
+++ config/wezterm/.wezterm.lua
@@ -9,6 +9,7 @@ function Scheme_for_appearance(appearance)
 end
 
 return {
+  -- fonts
   font = wezterm.font_with_fallback {
     {
       family = 'Iosevka',
@@ -16,8 +17,11 @@ return {
       harfbuzz_features = { "calt", "liga", "dlig", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" },
     },
   },
+  font_size = 16.0;
+  line_height = 1.0,
+  -- theme
   color_scheme = Scheme_for_appearance(wezterm.gui.get_appearance()),
-  hide_tab_bar_if_only_one_tab = true,
+  -- window and UI
   window_padding = {
     left   = '2cell',
     right  = '2cell',
@@ -25,10 +29,12 @@ return {
     bottom = '1cell'
   },
   window_decorations = "RESIZE",
+  window_close_confirmation = 'NeverPrompt',
   adjust_window_size_when_changing_font_size = false,
-  line_height = 1.1,
-  term = "wezterm",
+  hide_tab_bar_if_only_one_tab = true,
+  -- keyboard
   send_composed_key_when_left_alt_is_pressed = false,
   send_composed_key_when_right_alt_is_pressed = false,
-  window_close_confirmation = 'NeverPrompt'
+  -- term
+  term = "wezterm",
 }
