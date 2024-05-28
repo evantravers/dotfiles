6fa53891c2d5e9e9ead6dddf9b3a458505984967
Evan Travers <evantravers@gmail.com>
2024-05-28 14:50:16 -0500

    Flash isn't working right
    I really liked this plugin, just don't have the time to play with the
    configuration.

--- home/nvim.nix
+++ home/nvim.nix
@@ -423,49 +423,7 @@
           '';
         };
       }
-      {
-        plugin = flash-nvim;
-        type = "lua";
-        config = ''
-        require("flash").setup({
-          event = "VeryLazy",
-          opts = {
-            modes = {
-              char = {
-                enabled = false
-              }
-            }
-          },
-          keys = {
-            {
-              "s",
-              mode = { "n", "x", "o" },
-              function()
-                -- default options: exact mode, multi window, all directions, with a backdrop
-                require("flash").jump()
-              end,
-              desc = "Flash",
-            },
-            {
-              "S",
-              mode = { "n", "o", "x" },
-              function()
-                require("flash").treesitter()
-              end,
-              desc = "Flash Treesitter",
-            },
-            {
-              "r",
-              mode = "o",
-              function()
-                require("flash").remote()
-              end,
-              desc = "Remote Flash",
-            },
-          }
-        })
-        '';
-      }
+      flash-nvim
     ];
   };
 }
