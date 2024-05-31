f4c6c80dc16db91fc3eb315d25ef9a782b84803e
Evan Travers <evantravers@gmail.com>
2024-05-31 10:18:50 -0500

    remove unused bindings from the expressions
    I'm just chasing lsp warnings honestly. No clue if this is a good idea
    or not.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,7 +2,7 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   imports = [
