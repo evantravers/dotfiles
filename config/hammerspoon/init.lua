228754b6b42a08ffab67b2833265b58e047e175b
Evan Travers <evantravers@gmail.com>
2024-07-03 10:38:27 -0500

    Just cleaning up unused stuff

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -3,14 +3,151 @@ hs.loadSpoon('Headspace'):start()
 hs.loadSpoon('Teamz'):start()
 hs.loadSpoon('HyperModal')
 
-IsDocked = function()
-  return hs.fnutils.some(hs.usb.attachedDevices(), function(device)
-    return device.productName == "CalDigit Thunderbolt 3 Audio"
-  end)
-end
-
 Config = {}
-Config.applications = require('configApplications')
+Config.applications = {
+  ['com.raycast.macos'] = {
+    bundleID = 'com.raycast.macos',
+    localBindings = {'c', 'space'}
+  },
+  ['com.github.wez.wezterm'] = {
+    bundleID = 'com.github.wez.wezterm',
+    hyperKey = 'j',
+    tags = {'coding'}
+  },
+  ['com.brave.Browser'] = {
+    bundleID = 'com.brave.Browser',
+    tags = {'browsers'},
+  },
+  ['org.mozilla.firefox'] = {
+    bundleID = 'org.mozilla.firefox',
+    tags = {'browsers'}
+  },
+  ['com.apple.Safari'] = {
+    bundleID = 'com.apple.Safari',
+    tags = {'browsers'}
+  },
+  ['com.google.Chrome'] = {
+    bundleID = 'com.google.Chrome',
+    tags = {'browsers'}
+  },
+  ['com.microsoft.teams2'] = {
+    bundleID = 'com.microsoft.teams2',
+    tags = {'communication', 'chat'}
+  },
+  ['com.apple.mail'] = {
+    bundleID = 'com.apple.mail',
+    hyperKey = 'e',
+    tags = {'communication', 'distraction'}
+  },
+  ['com.flexibits.fantastical2.mac'] = {
+    bundleID = 'com.flexibits.fantastical2.mac',
+    hyperKey = 'y',
+    localBindings = {'/'},
+    tags = {'planning', 'review', 'calendar'},
+    whitelisted = true,
+  },
+  ['com.apple.finder'] = {
+    bundleID = 'com.apple.finder',
+    hyperKey = 'f'
+  },
+  ['com.hnc.Discord'] = {
+    bundleID = 'com.hnc.Discord',
+    tags = {'distraction', 'chat'},
+  },
+  ['com.tinyspeck.slackmacgap'] = {
+    bundleID = 'com.tinyspeck.slackmacgap',
+    tags = {'distraction', 'communication', 'chat'},
+  },
+  ['com.tapbots.Tweetbot3Mac'] = {
+    bundleID = 'com.tapbots.Tweetbot3Mac',
+    tags = {'distraction', 'socialmedia'},
+  },
+  ['com.culturedcode.ThingsMac'] = {
+    bundleID = 'com.culturedcode.ThingsMac',
+    hyperKey = 't',
+    tags = {'planning', 'review', 'tasks'},
+    whitelisted = true,
+    localBindings = {',', '.'},
+  },
+  ['com.agiletortoise.Drafts-OSX'] = {
+    bundleID = 'com.agiletortoise.Drafts-OSX',
+    hyperKey ='d',
+    tags = {'review', 'writing', 'research', 'notes'},
+    whitelisted = true,
+    localBindings = {'x', 'n'}
+  },
+  ['com.joehribar.toggl'] = {
+    bundleID = 'com.joehribar.toggl',
+    hyperKey = 'r'
+  },
+  ['com.ideasoncanvas.mindnode.macos'] = {
+    bundleID = 'com.ideasoncanvas.mindnode.macos',
+    tags = {'research'},
+  },
+  ['com.apple.MobileSMS'] = {
+    bundleID = 'com.apple.MobileSMS',
+    tags = {'communication', 'distraction', 'personal'},
+  },
+  ['com.valvesoftware.steam'] = {
+    bundleID = 'com.valvesoftware.steam',
+    tags = {'distraction'}
+  },
+  ['net.battle.app'] = {
+    bundleID = 'net.battle.app',
+    tags = {'distraction'}
+  },
+  ['com.spotify.client'] = {
+    bundleID = 'com.spotify.client'
+  },
+  ['com.figma.Desktop'] = {
+    bundleID = 'com.figma.Desktop',
+    tags = {'design'},
+  },
+  ['com.reederapp.5.macOS'] = {
+    bundleID = 'com.reederapp.5.macOS',
+    tags = {'distraction'},
+  },
+  ['md.obsidian'] = {
+    bundleID = 'md.obsidian',
+    hyperKey = 'g',
+    tags = {'research', 'notes'},
+  },
+  ['us.zoom.xos'] = {
+    bundleID = 'us.zoom.xos',
+  },
+  ['org.whispersystems.signal-desktop'] = {
+    bundleID = 'org.whispersystems.signal-desktop',
+    tags = {'distraction', 'communication', 'personal'}
+  },
+  ['ru.keepcoder.Telegram'] = {
+    bundleID = 'ru.keepcoder.Telegram',
+    tags = {'distraction', 'communication', 'personal'}
+  },
+  ['com.surteesstudios.Bartender'] = {
+    bundleID = 'com.surteesstudios.Bartender',
+    localBindings = {'b'}
+  },
+  ['company.thebrowser.Browser'] = {
+    bundleID = 'company.thebrowser.Browser',
+    tags = {'browsers'}
+  },
+  ['com.superultra.Homerow'] = {
+    bundleID = 'com.superultra.Homerow',
+    localBindings = {'l'}
+  },
+  ['com.flexibits.cardhop.mac'] = {
+    bundleID = 'com.flexibits.cardhop.mac',
+    localBindings = {'u'}
+  },
+  ['com.goodsnooze.MacWhisper'] = {
+    bundleID = 'com.goodsnooze.MacWhisper',
+    localBindings = {'a'}
+  },
+  ['com.goodsnooze.MenuGPT'] = {
+    bundleID = 'com.goodsnooze.MenuGPT',
+    localBindings = {'p'}
+  }
+}
 
 Hyper = spoon.Hyper
 
@@ -272,23 +409,6 @@ Hyper:bind({}, 'h', nil, function()
 end)
 
 require('browserSnip')
-
--- sync tags to OSX
--- Currently unused, because of permissions
-Config.appsync = function()
-  hs.fnutils.map(Config.applications, function(a)
-    if a["tags"] and a["bundleID"] then
-      local app_path = hs.application.pathForBundleID(a["bundleID"]) -- may not be installed!
-      if app_path then
-        if not a["bundleID"]:find("apple") then
-          print("attempting " .. app_path)
-          print(hs.inspect(a["tags"]))
-          hs.fs.tagsSet(app_path, a["tags"])
-        end
-      end
-    end
-  end)
-end
 
 -- change audio settings based on output
 hs.audiodevice.watcher.setCallback(function(event)
