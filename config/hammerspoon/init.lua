594543713a1d3847b3921aa600efee6627822541
Evan Travers <evantravers@gmail.com>
2024-07-03 11:30:18 -0500

    Major simplification of hammerspoon
    - I'm using OSX tags for Headspace, so I don't need the tags except for
      HyperGroups, that'll come later.
    - I was scanning around this giant file to find bindings, so just bring
      them all together.

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -3,163 +3,53 @@ hs.loadSpoon('Headspace'):start()
 hs.loadSpoon('Teamz'):start()
 hs.loadSpoon('HyperModal')
 
-Config = {}
-Config.applications = {
-  ['com.raycast.macos'] = {
-    bundleID = 'com.raycast.macos',
-    localBindings = {'c', 'space'}
+Tags = {
+  ['browsers'] = {
+    'com.brave.Browser',
+    'org.mozilla.firefox',
+    'com.apple.Safari',
+    'com.google.Chrome'
   },
-  ['com.github.wez.wezterm'] = {
-    bundleID = 'com.github.wez.wezterm',
-    hyperKey = 'j',
-    tags = {'coding'}
+  ['distraction'] = {
+    'com.hnc.Discord',
+    'com.tinyspeck.slackmacgap',
+    'com.tapbots.Tweetbot3Mac'
   },
-  ['com.brave.Browser'] = {
-    bundleID = 'com.brave.Browser',
-    tags = {'browsers'},
-  },
-  ['org.mozilla.firefox'] = {
-    bundleID = 'org.mozilla.firefox',
-    tags = {'browsers'}
-  },
-  ['com.apple.Safari'] = {
-    bundleID = 'com.apple.Safari',
-    tags = {'browsers'}
-  },
-  ['com.google.Chrome'] = {
-    bundleID = 'com.google.Chrome',
-    tags = {'browsers'}
-  },
-  ['com.microsoft.teams2'] = {
-    bundleID = 'com.microsoft.teams2',
-    tags = {'communication', 'chat'}
-  },
-  ['com.apple.mail'] = {
-    bundleID = 'com.apple.mail',
-    hyperKey = 'e',
-    tags = {'communication', 'distraction'}
-  },
-  ['com.flexibits.fantastical2.mac'] = {
-    bundleID = 'com.flexibits.fantastical2.mac',
-    hyperKey = 'y',
-    localBindings = {'/'},
-    tags = {'planning', 'review', 'calendar'},
-    whitelisted = true,
-  },
-  ['com.apple.finder'] = {
-    bundleID = 'com.apple.finder',
-    hyperKey = 'f'
-  },
-  ['com.hnc.Discord'] = {
-    bundleID = 'com.hnc.Discord',
-    tags = {'distraction', 'chat'},
-  },
-  ['com.tinyspeck.slackmacgap'] = {
-    bundleID = 'com.tinyspeck.slackmacgap',
-    tags = {'distraction', 'communication', 'chat'},
-  },
-  ['com.tapbots.Tweetbot3Mac'] = {
-    bundleID = 'com.tapbots.Tweetbot3Mac',
-    tags = {'distraction', 'socialmedia'},
-  },
-  ['com.culturedcode.ThingsMac'] = {
-    bundleID = 'com.culturedcode.ThingsMac',
-    hyperKey = 't',
-    tags = {'planning', 'review', 'tasks'},
-    whitelisted = true,
-    localBindings = {',', '.'},
-  },
-  ['com.agiletortoise.Drafts-OSX'] = {
-    bundleID = 'com.agiletortoise.Drafts-OSX',
-    hyperKey ='d',
-    tags = {'review', 'writing', 'research', 'notes'},
-    whitelisted = true,
-    localBindings = {'x', 'n'}
-  },
-  ['com.joehribar.toggl'] = {
-    bundleID = 'com.joehribar.toggl',
-    hyperKey = 'r'
-  },
-  ['com.ideasoncanvas.mindnode.macos'] = {
-    bundleID = 'com.ideasoncanvas.mindnode.macos',
-    tags = {'research'},
-  },
-  ['com.apple.MobileSMS'] = {
-    bundleID = 'com.apple.MobileSMS',
-    tags = {'communication', 'distraction', 'personal'},
-  },
-  ['com.valvesoftware.steam'] = {
-    bundleID = 'com.valvesoftware.steam',
-    tags = {'distraction'}
-  },
-  ['net.battle.app'] = {
-    bundleID = 'net.battle.app',
-    tags = {'distraction'}
-  },
-  ['com.spotify.client'] = {
-    bundleID = 'com.spotify.client'
-  },
-  ['com.figma.Desktop'] = {
-    bundleID = 'com.figma.Desktop',
-    tags = {'design'},
-  },
-  ['com.reederapp.5.macOS'] = {
-    bundleID = 'com.reederapp.5.macOS',
-    tags = {'distraction'},
-  },
-  ['md.obsidian'] = {
-    bundleID = 'md.obsidian',
-    hyperKey = 'g',
-    tags = {'research', 'notes'},
-  },
-  ['us.zoom.xos'] = {
-    bundleID = 'us.zoom.xos',
-  },
-  ['org.whispersystems.signal-desktop'] = {
-    bundleID = 'org.whispersystems.signal-desktop',
-    tags = {'distraction', 'communication', 'personal'}
-  },
-  ['ru.keepcoder.Telegram'] = {
-    bundleID = 'ru.keepcoder.Telegram',
-    tags = {'distraction', 'communication', 'personal'}
-  },
-  ['com.surteesstudios.Bartender'] = {
-    bundleID = 'com.surteesstudios.Bartender',
-    localBindings = {'b'}
-  },
-  ['company.thebrowser.Browser'] = {
-    bundleID = 'company.thebrowser.Browser',
-    tags = {'browsers'}
-  },
-  ['com.superultra.Homerow'] = {
-    bundleID = 'com.superultra.Homerow',
-    localBindings = {'l'}
-  },
-  ['com.flexibits.cardhop.mac'] = {
-    bundleID = 'com.flexibits.cardhop.mac',
-    localBindings = {'u'}
-  },
-  ['com.goodsnooze.MacWhisper'] = {
-    bundleID = 'com.goodsnooze.MacWhisper',
-    localBindings = {'a'}
-  },
-  ['com.goodsnooze.MenuGPT'] = {
-    bundleID = 'com.goodsnooze.MenuGPT',
-    localBindings = {'p'}
+  ['communication'] = {
+    'com.microsoft.teams2',
+    'com.apple.mail'
   }
+}
+-- bundleID, global, local
+Bindings = {
+  {'com.agiletortoise.Drafts-OSX', 'd', {'x', 'n'}},
+  {'com.apple.finder', 'f', nil},
+  {'com.apple.mail', 'e', nil},
+  {'com.culturedcode.ThingsMac', 't', {',', '.'}},
+  {'com.flexibits.cardhop.mac', nil, {'u'}},
+  {'com.flexibits.fantastical2.mac', 'y', {'/'}},
+  {'com.github.wez.wezterm', 'j', nil},
+  {'com.goodsnooze.MenuGPT', nil, {'p'}},
+  {'com.goodsnoze.MacWhisper', nil, {'a'}},
+  {'com.joehribar.toggl', 'r', nil},
+  {'com.raycast.macos', nil, {'c', 'space'}},
+  {'com.superultra.Homerow', nil, {'l'}},
+  {'com.surteesstudios.Bartender', nil, {'b'}},
+  {'md.obsidian', 'g', nil},
 }
 
 Hyper = spoon.Hyper
 
 Hyper:bindHotKeys({hyperKey = {{}, 'F19'}})
 
-hs.fnutils.each(Config.applications, function(appConfig)
-  if appConfig.hyperKey then
-    Hyper:bind({}, appConfig.hyperKey, function() hs.application.launchOrFocusByBundleID(appConfig.bundleID) end)
+hs.fnutils.each(Bindings, function(bindingTable)
+  local bundleID, globalBind, localBinds = table.unpack(bindingTable)
+  if globalBind then
+    Hyper:bind({}, globalBind, function() hs.application.launchOrFocusByBundleID(bundleID) end)
   end
-  if appConfig.localBindings then
-    hs.fnutils.each(appConfig.localBindings, function(key)
-      Hyper:bindPassThrough(key, appConfig.bundleID)
+  if localBinds then
+    hs.fnutils.each(localBinds, function(key)
+      Hyper:bindPassThrough(key, bundleID)
     end)
   end
 end)
