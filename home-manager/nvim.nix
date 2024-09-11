b8d8e8648201a462057a21edc809faa624b8c79c
Evan Travers <evantravers@gmail.com>
2024-09-11 16:21:13 -0500

    Fix localleader

--- home-manager/nvim.nix
+++ home-manager/nvim.nix
@@ -344,7 +344,7 @@
               vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
               vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
               vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-              vim.keymap.set('n', '<space>f', function()
+              vim.keymap.set('n', '<localleader>f', function()
                 vim.lsp.buf.format { async = true }
               end, opts)
             end,
