275d924c9a54944127011b22da73b8224c19d6a6
Evan Travers <evantravers@gmail.com>
2024-09-16 14:29:23 -0500

    Stripping stuff down!
    - Replace lualine with mini.statusline
    - Remove git-messenger (I hadn't used it.)

--- home-manager/nvim.nix
+++ home-manager/nvim.nix
@@ -36,17 +36,6 @@
         '';
       }
       {
-        plugin = lualine-nvim;
-        type = "lua";
-        config = ''
-          require('lualine').setup {
-            options = {
-              theme = "zenbones"
-            }
-          }
-        '';
-      }
-      {
         plugin = oil-nvim;
         type = "lua";
         config = ''
@@ -58,7 +47,6 @@
           vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory" })
         '';
       }
-      git-messenger-vim
       {
         plugin = gitsigns-nvim;
         type = "lua";
@@ -87,7 +75,7 @@
           src = pkgs.fetchFromGitHub {
             owner = "f-person";
             repo = "auto-dark-mode.nvim";
-            rev = "76e8d40d1e1544bae430f739d827391cbcb42fcc";
+            rev = "14cad96b80a07e9e92a0dcbe235092ed14113fb2";
             hash = "sha256-uJ4LxczgWl4aQCFuG4cR+2zwhNo7HB6R7ZPTdgjvyfY=";
           };
         };
@@ -104,7 +92,6 @@
         })
         '';
       }
-      noice-nvim
       # treesitter
       {
         plugin = nvim-treesitter.withAllGrammars;
@@ -131,12 +118,12 @@
         require('tree-pairs').setup()
         '';
       }
-      # targets and textobjects
       {
         plugin = mini-nvim;
         type = "lua";
         config = ''
           require('mini.ai').setup()
+          require('mini.statusline').setup()
         '';
       }
       {
@@ -149,95 +136,6 @@
             keymaps = {
               node_incremental = "v",
               node_decremental = "V",
-            },
-          },
-          textobjects = {
-            select = {
-              enable = true,
-
-              -- Automatically jump forward to textobj, similar to targets.vim
-              lookahead = true,
-
-              keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                -- You can optionally set descriptions to the mappings (used in the desc parameter of
-                -- nvim_buf_set_keymap) which plugins like which-key display
-                ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-                -- You can also use captures from other query groups like `locals.scm`
-                ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-              },
-              -- You can choose the select mode (default is charwise 'v')
-              --
-              -- Can also be a function which gets passed a table with the keys
-              -- * query_string: eg '@function.inner'
-              -- * method: eg 'v' or 'o'
-              -- and should return the mode ('v', 'V', or '<c-v>') or a table
-              -- mapping query_strings to modes.
-              selection_modes = {
-                ['@parameter.outer'] = 'v', -- charwise
-                ['@function.outer'] = 'V', -- linewise
-                ['@class.outer'] = '<c-v>', -- blockwise
-              },
-              -- If you set this to `true` (default is `false`) then any textobject is
-              -- extended to include preceding or succeeding whitespace. Succeeding
-              -- whitespace has priority in order to act similarly to eg the built-in
-              -- `ap`.
-              --
-              -- Can also be a function which gets passed a table with the keys
-              -- * query_string: eg '@function.inner'
-              -- * selection_mode: eg 'v'
-              -- and should return true or false
-              include_surrounding_whitespace = true,
-            },
-            move = {
-              enable = true,
-              set_jumps = true, -- whether to set jumps in the jumplist
-              goto_next_start = {
-                ["]m"] = "@function.outer",
-                ["]]"] = { query = "@class.outer", desc = "Next class start" },
-                --
-                -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queries.
-                ["]o"] = "@loop.*",
-                -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
-                --
-                -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
-                -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-                ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
-                ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-              },
-              goto_next_end = {
-                ["]M"] = "@function.outer",
-                ["]["] = "@class.outer",
-              },
-              goto_previous_start = {
-                ["[m"] = "@function.outer",
-                ["[["] = "@class.outer",
-              },
-              goto_previous_end = {
-                ["[M"] = "@function.outer",
-                ["[]"] = "@class.outer",
-              },
-              -- Below will go to either the start or the end, whichever is closer.
-              -- Use if you want more granular movements
-              -- Make it even more gradual by adding multiple queries and regex.
-              goto_next = {
-                ["]d"] = "@conditional.outer",
-              },
-              goto_previous = {
-                ["[d"] = "@conditional.outer",
-              }
-            },
-            swap = {
-              enable = true,
-              swap_next = {
-                ["<leader>a"] = "@parameter.inner",
-              },
-              swap_previous = {
-                ["<leader>A"] = "@parameter.inner",
-              },
             },
           },
         }
@@ -265,9 +163,7 @@
         '';
       }
       # operators
-      vim-abolish
       vim-eunuch
-      vim-projectionist
       vim-ragtag
       vim-repeat
       vim-speeddating
@@ -511,7 +407,7 @@
           })
         '';
       }
-      completion-nvim
+      coq_nvim
       {
         plugin = telescope-nvim;
         type = "lua";
