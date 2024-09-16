abf224274a8ded580901515ab5020efe0e99e221
Evan Travers <evantravers@gmail.com>
2024-09-16 12:19:06 -0500

    Try mini.ai

--- home-manager/nvim.nix
+++ home-manager/nvim.nix
@@ -34,6 +34,14 @@
           }
         '';
       }
+      # targets and textobjects
+      {
+        plugin = mini-nvim;
+        type = "lua";
+        config = ''
+          require('mini.ai').setup()
+        '';
+      }
       {
         plugin = nvim-treesitter-textobjects;
         type = "lua";
