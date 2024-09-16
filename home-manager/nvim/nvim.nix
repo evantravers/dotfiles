7a1687a29f3cd9f958b7d8debd164aa2c06c3a2f
Evan Travers <evantravers@gmail.com>
2024-09-16 16:16:57 -0500

    I don't miss not having completion atm.

--- home-manager/nvim/nvim.nix
+++ home-manager/nvim/nvim.nix
@@ -155,7 +155,6 @@
           require('mini.align').setup()      -- aligning
           require('mini.bracketed').setup()  -- unimpaired bindings with TS
           require('mini.comment').setup()    -- TS-wise comments
-          require('mini.completion').setup() -- simple LSP completions
           require('mini.icons').setup()      -- minimal icons
           require('mini.jump').setup()       -- fFtT work past a line
           require('mini.pairs').setup()      -- pair brackets
