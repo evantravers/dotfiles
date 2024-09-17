9cfeb302def73fe2de5300a63aef8f193881b6cb
Evan Travers <evantravers@gmail.com>
2024-09-17 10:30:42 -0500

    Go back
    I'm used to starship now

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -62,17 +62,12 @@
         command_timeout = 100;
         format = "[$all](dimmed white)";
 
-        directory = {
-          style = "bold bright-white";
-        };
-
         character = {
           success_symbol = "[❯](dimmed green)";
           error_symbol = "[❯](dimmed red)";
         };
 
         git_branch = {
-          style = "bold bright-white";
           ignore_branches = [ "master" "main" ];
         };
 
