0ded0404227d1929429618206f57211c02bf4e5a
Evan Travers <evantravers@gmail.com>
2024-09-28 15:00:36 -0500

    Update nixd with recommended config

--- home-manager/nvim/lsp.lua
+++ home-manager/nvim/lsp.lua
@@ -45,7 +45,28 @@ lspconfig.solargraph.setup({
     }
   }
 })
-lspconfig.nixd.setup {}
+local nvim_lsp = require("lspconfig")
+nvim_lsp.nixd.setup({
+  cmd = { "nixd" },
+  settings = {
+    nixd = {
+      nixpkgs = {
+        expr = "import <nixpkgs> { }",
+      },
+      formatting = {
+        command = { "nixfmt" },
+      },
+      options = {
+        nixos = {
+          expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.k-on.options',
+        },
+        home_manager = {
+          expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."ruixi@k-on".options',
+        },
+      },
+    },
+  },
+})
 lspconfig.lua_ls.setup {
   settings = {
     Lua = {
