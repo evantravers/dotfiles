1d9153c8dd759eebf1c710975de02c7761b4e70d
Evan Travers <evantravers@gmail.com>
2024-10-06 21:35:52 -0500

    Throw claude.ai at it

--- flake.nix
+++ flake.nix
@@ -1,54 +1,42 @@
 {
   description = "Evan's Nix System Configuration";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     flake-utils.url = "github:numtide/flake-utils";
   };
-
-  outputs = {
-    darwin,
-    home-manager,
-    ...
-  } @ inputs: let
-      darwinSystem = {host, user, arch ? "aarch64-darwin"}: {
-        ${host} = darwin.lib.darwinSystem {
-          system = ${arch};
-          modules = [
-            ./darwin/darwin.nix
-            home-manager.darwinModules.home-manager
-            {
-              _module.args = { inherit inputs; };
-              home-manager = {
-                users.${user} = import ./home-manager;
-              };
-              # FIXME: feels like the below could be two lines above
-              users.users.${user}.home = "/Users/${user}";
-              nix.settings.trusted-users = [ user ];
-            }
-          ];
-        };
+  outputs = { nixpkgs, darwin, home-manager, nixos-wsl, ... } @ inputs: let
+    darwinSystem = {host, user, arch ? "aarch64-darwin"}: 
+      darwin.lib.darwinSystem {
+        system = arch;
+        modules = [
+          ./darwin/darwin.nix
+          home-manager.darwinModules.home-manager
+          {
+            _module.args = { inherit inputs; };
+            home-manager = {
+              users.${user} = import ./home-manager;
+            };
+            users.users.${user}.home = "/Users/${user}";
+            nix.settings.trusted-users = [ user ];
+          }
+        ];
       };
   in
   {
     nixosConfigurations = {
-      "nixos" = inputs.nixpkgs.lib.nixosSystem {
+      nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           nixos-wsl.nixosModules.wsl
@@ -56,24 +44,24 @@
           ./.config/wsl
           home-manager.nixosModules.home-manager
           {
-            home-manager.users.nixos = import ./home-manager;
+            home-manager = {
+              users.nixos = import ./home-manager;
+            };
+            nix.settings.trusted-users = [ "nixos" ];
           }
         ];
       };
-      nix.settings.trusted-users = [ "nixos" ];
     };
-
-
     darwinConfigurations = {
-      darwinSystem({
+      "G2157QVFX1" = darwinSystem {
         host = "G2157QVFX1";
         user = "etravers";
-      });
-      darwinSystem({
+      };
+      "Evans-MacBook-Pro" = darwinSystem {
         host = "Evans-MacBook-Pro";
         user = "evan";
         arch = "x86_64-darwin";
-      });
+      };
     };
   };
 }
