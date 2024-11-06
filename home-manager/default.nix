21de3842f27df5168194044cd957ffbf4ab4887f
Evan Travers <evantravers@gmail.com>
2024-11-05 18:39:57 -0600

    Cleaning up the file

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -45,26 +45,18 @@
   };
 
   programs = {
-    # Use fish
     fish = {
       enable = true;
-
       interactiveShellInit = ''
         set fish_greeting # N/A
       '';
-
-      plugins = [
-      ];
     };
 
     direnv = {
       enable = true;
-
       nix-direnv.enable = true;
     };
 
-    jujutsu = {
-      enable = true;
-    };
+    jujutsu.enable = true;
   };
 }
