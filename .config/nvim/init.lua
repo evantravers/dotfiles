6cb144b40217fcdc63e16ade8539a5bccb3d40ba
Evan Travers <evantravers@gmail.com>
2025-03-28 15:39:20 -0500

    Add LSP completion and diagnostic virtual_lines
    https://gpanders.com/blog/whats-new-in-neovim-0-11/

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -41,3 +41,18 @@ vim.keymap.set('n',
   '<leader>fs',
   '1z=',
   {noremap = true, silent = true, desc = "Fix spelling under cursor"})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+  end,
+})
+
+vim.diagnostic.config({
+  virtual_lines = {
+   current_line = true,
+  },
+})
