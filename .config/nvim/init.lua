4829b1c477922331e929f68445fb7a8f49e3d952
Evan Travers <evantravers@gmail.com>
2025-03-29 10:26:26 -0500

    Remove options from other person's config. *facepalm*

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -71,14 +71,6 @@ vim.lsp.config.nixd = {
       formatting = {
         command = { "nixfmt" },
       },
-      options = {
-        nixos = {
-          expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.k-on.options',
-        },
-        home_manager = {
-          expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."ruixi@k-on".options',
-        },
-      },
     },
   }
 }
