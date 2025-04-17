52260739392a190377dead362f6f2c5f032f0d4a
Evan Travers <evantravers@gmail.com>
2025-04-17 09:50:38 -0500

    mv darwin/darwin.nix -> users/evantravers/darwin.nix

--- users/evantravers/darwin.nix
+++ users/evantravers/darwin.nix
@@ -1,34 +1,9 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 
 {
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages =
-    [
-      pkgs.home-manager
-    ];
-
-  # Use a custom configuration.nix location.
-  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
-  environment.darwinConfig = "$HOME/src/github.com/evantravers/dotfiles/darwin";
-
-  # Auto upgrade nix package and the daemon service.
-  nix.enable = false;
-
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  programs = {
-    gnupg.agent.enable = true;
-    zsh.enable = true;  # default shell on catalina
-  };
-
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-  system.stateVersion = 4;
-
-  fonts.packages = [
-    pkgs.atkinson-hyperlegible
-    pkgs.jetbrains-mono
-  ];
+  # Enable fish and zsh
+  programs.zsh.enable = true;
+  programs.fish.enable = true;
 
   services = {
     aerospace = {
@@ -124,6 +99,10 @@
     };
   };
 
+  fonts.packages = [
+    pkgs.atkinson-hyperlegible
+    pkgs.jetbrains-mono
+  ];
   system = {
     defaults = {
       dock = {
@@ -151,4 +130,8 @@
       remapCapsLockToControl = true;
     };
   };
+
+  security.sudo.extraConfig = ''
+  Defaults timestamp_timeout=15
+  '';
 }
