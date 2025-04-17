0044448995a1a15f6b3b0b87cce4ece908545083
Evan Travers <evantravers@gmail.com>
2025-04-17 10:10:02 -0500

    nix flake update

--- users/evantravers/nixos.nix
+++ users/evantravers/nixos.nix
@@ -1,8 +1,15 @@
 { pkgs, inputs, ... }:
 
 {
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  system.stateVersion = "24.11";
+
   # Since we're using fish as our shell
   programs.fish.enable = true;
+  users.defaultUserShell = pkgs.fish;
+
+  time.timeZone = "America/Chicago";
 
   users.users.evantravers = {
     isNormalUser = true;
@@ -10,6 +17,5 @@
     # extraGroups = [ "docker" "lxd" "wheel" ];
     shell = pkgs.fish;
   };
-
 }
 
