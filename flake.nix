d67c2f6331482cedb132b602373a37758b138385
Evan Travers <evantravers@gmail.com>
2025-04-28 20:36:51 -0500

    Switch to 24.11
    I'm not using ollama that religiously at the moment, and if I need it I
    can get it through brew anyway.

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
