929dad2f7c472cdc58e9b3bfe1f8692a6d21371d
Evan Travers <evantravers@gmail.com>
2025-05-02 23:03:11 -0500

    Add starship prompt for jj

--- users/evantravers/starship.nix
+++ users/evantravers/starship.nix
@@ -16,6 +16,51 @@
         format = "([$all_status$ahead_behind]($style) )";
       };
 
+      # custom module for jj status
+      # https://github.com/jj-vcs/jj/wiki/Starship
+      custom.jj = {
+        ignore_timeout = true;
+        description = "The current jj status";
+        detect_folders = [".jj"];
+        symbol = "🥋 ";
+        command = ''
+        jj log --revisions @ --no-graph --ignore-working-copy --color always --limit 1 --template '
+          separate(" ",
+            change_id.shortest(4),
+            bookmarks,
+            "|",
+            concat(
+              if(conflict, "💥"),
+              if(divergent, "🚧"),
+              if(hidden, "👻"),
+              if(immutable, "🔒"),
+            ),
+            raw_escape_sequence("\x1b[1;32m") ++ if(empty, "(empty)"),
+            raw_escape_sequence("\x1b[1;32m") ++ coalesce(
+              truncate_end(29, description.first_line(), "…"),
+              "(no description set)",
+            ) ++ raw_escape_sequence("\x1b[0m"),
+          )
+        '
+        '';
+      };
+
+      # optionally disable git modules
+      git_state.disabled = true;
+
+      git_commit.disabled = true;
+
+      git_metrics.disabled = true;
+
+      git_branch.disabled = true;
+
+      # re-enable git_branch as long as we're not in a jj repo
+      custom.git_branch = {
+        when = true;
+        command = "jj root >/dev/null 2>&1 || starship module git_branch";
+        description = "Only show git_branch if we're not in a jj repo";
+      };
+
       elixir.symbol    = " ";
       lua.symbol       = "󰢱 ";
       nix_shell.symbol = " ";
