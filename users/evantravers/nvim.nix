ab0ea277b74897e7ee954449814ba7c8a32f4abb
Evan Travers <evantravers@gmail.com>
2025-07-06 08:06:39 -0500

    Use modified bindings for codecompanion

--- users/evantravers/nvim.nix
+++ users/evantravers/nvim.nix
@@ -318,6 +318,13 @@
               history = { enabled = true };
             }
           })
+
+          vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true, desc = "✨ Actions" })
+          vim.keymap.set({ "n", "v" }, "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true, desc = "✨ Toggle Chat" })
+          vim.keymap.set("v", "<LocalLeader>c", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true, desc = "✨ Add to Chat" })
+
+          -- Expand 'cc' into 'CodeCompanion' in the command line
+          vim.cmd([[cab cc CodeCompanion]])
         '';
       }
     ];
