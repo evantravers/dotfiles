4e3215fc826f7b81be7259102fcc5724fa0ae53c
Evan Travers <evantravers@gmail.com>
2025-07-14 10:26:15 -0500

    Set SSH_AUTH_SOCK on darwin only
    I was using sessionVariables, but I think it was interpolating
    `...Group\ Containers...` to `...Group Containers...`
    
    Tom told me about the gnupg method, so I'm using that.

--- users/evantravers/darwin.nix
+++ users/evantravers/darwin.nix
@@ -25,6 +25,10 @@
     pkgs.zed-editor
   ];
 
+  environment.extraInit = ''
+  export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+  '';
+
   services = {
     aerospace = {
       enable = true;
