2b4d287abfab2ca556535b954dfd2da217a66884
Evan Travers <evantravers@gmail.com>
2025-08-25 10:16:28 -0500

    Add himalaya
    It seems like there's an error with home-manager.programs.himalaya that
    causes the settings to always be overwritten by an empty attrset.
    
    https://github.com/nix-community/home-manager/issues/6190

--- users/evantravers/home-manager.nix
+++ users/evantravers/home-manager.nix
@@ -48,6 +48,29 @@
   }
   '';
 
+  # TODO: Put this in attrset as soon as home-manager is fixed
+  xdg.configFile."himalaya/config.toml".text = ''
+  [accounts.gmail]
+  default = true
+  email = "evantravers@gmail.com"
+  display-name = "evantravers"
+  downloads-dir = "/Users/evantravers/Downloads"
+  backend.type = "imap"
+  backend.host = "imap.gmail.com"
+  backend.port = 993
+  backend.login = "evantravers@gmail.com"
+  backend.encryption.type = "tls"
+  backend.auth.type = "password"
+  backend.auth.command = "op read op://Private/a3v65jhzsq4lpiunlcf6fceesa/password"
+  message.send.backend.type = "smtp"
+  message.send.backend.host = "smtp.gmail.com"
+  message.send.backend.port = 465
+  message.send.backend.login = "evantravers@gmail.com"
+  message.send.backend.encryption.type = "tls"
+  message.send.backend.auth.type = "password"
+  message.send.backend.auth.command = "op read op://Private/a3v65jhzsq4lpiunlcf6fceesa/password"
+  '';
+
   home = {
     stateVersion = "24.05"; # Please read the comment before changing.
 
@@ -60,6 +83,7 @@
       gh
       gum
       harper
+      himalaya
       lua-language-server
       markdown-oxide
       nixd
