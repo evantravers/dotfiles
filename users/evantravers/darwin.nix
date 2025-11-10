2d638ecee64fd18f999df2ca9f8b0e0d0dd8360b
Evan Travers <evantravers@gmail.com>
2025-11-10 16:54:28 -0600

    Remove HOMEBREW_AUTO_UPDATE_SECS

--- users/evantravers/darwin.nix
+++ users/evantravers/darwin.nix
@@ -48,10 +48,6 @@
   export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
   '';
 
-  environment.variables = {
-    HOMEBREW_AUTO_UPDATE_SECS = "86400";
-  };
-
   services = {
     aerospace = {
       enable = false;
