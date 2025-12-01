9a8c4e4a6f8c4bc29eea995ab7199f716d05df3d
Evan Travers <evantravers@gmail.com>
2025-11-30 19:51:55 -0600

    Bumping nix versions
    https://nixos.org/blog/announcements/2025/nixos-2511/

--- flake.nix
+++ flake.nix
@@ -2,16 +2,18 @@
   description = "Evan's Nix System Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     darwin = {
-      url = "github:LnL7/nix-darwin/nix-darwin-25.05";
+      # url = "github:LnL7/nix-darwin/nix-darwin-25.11";
+      # TODO: use the branch once #1647 is closed
+      url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
