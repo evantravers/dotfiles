812caa07d87a38075521dc9e09a9edc31cf81f25
Evan Travers <evantravers@gmail.com>
2025-12-15 10:44:34 -0600

    update codecompanion
    Fixing breaking changes, removing old plugins, removing code that no
    longer works.

--- users/evantravers/nvim-ai.nix
+++ users/evantravers/nvim-ai.nix
@@ -13,8 +13,8 @@
           src = pkgs.fetchFromGitHub {
             owner = "olimorris";
             repo = "codecompanion.nvim";
-            rev = "68fcda022fce25ee380e267d4b0fe98113b3957d";
-            sha256 = "sha256-J33VhN7XVo/DFXw+zIzeewMqV16rmt5ChUtLHtdlexE=";
+            rev = "e8b225e6a322b0bd1ae269d8032117a5500c27d8";
+            sha256 = "sha256-TFNX9vbBjEzs4UT2Cz4VPGBJh0qST9kph2UawcsdYjE=";
           };
           doCheck = false;
           checkPhase = ":";
@@ -26,7 +26,7 @@
           local ai_strategy = os.getenv("AI_STRATEGY") or "anthropic"
 
           require("codecompanion").setup({
-            strategies = {
+            interactions = {
               chat = {
                 adapter = ai_strategy,
                 keymaps = {
@@ -82,15 +82,6 @@
               }
             },
             extensions = {
-              history = {
-                enabled = true,
-                opts = {
-                  title_generation_opts = nil,
-                  title_generation_opts = {
-                    adapter = 'anthropic',
-                  },
-                }
-              }
             }
           })
 
@@ -102,62 +93,6 @@
           vim.cmd([[cab cc CodeCompanion]])
         '';
       }
-
-      # mini.diff for changes
-      # mini.notify to show LLM API call status
-      {
-        plugin = mini-nvim;
-        type = "lua";
-        config = ''
-          local diff = require('mini.diff') -- hunk management and highlight
-          diff.setup({source = diff.gen_source.none()})
-
-          local MiniNotify = require('mini.notify') -- notifications
-          local ids = {} -- CodeCompanion request ID --> MiniNotify notification ID
-          local group = vim.api.nvim_create_augroup("CodeCompanionMiniNotifyHooks", {})
-
-          local function format_status(ev)
-            local name = ev.data.adapter.formatted_name or ev.data.adapter.name
-            local msg = name .. " " .. ev.data.strategy .. " request..."
-            local level, hl_group = "INFO", "DiagnosticInfo"
-            if ev.data.status then
-              msg = msg .. ev.data.status
-              if ev.data.status ~= "success" then
-                level, hl_group = "ERROR", "DiagnosticError"
-              end
-            end
-            return msg, level, hl_group
-          end
-
-          vim.api.nvim_create_autocmd({ "User" }, {
-            pattern = "CodeCompanionRequestStarted",
-            group = group,
-            callback = function(ev)
-              local msg, level, hl_group = format_status(ev)
-              ids[ev.data.id] = MiniNotify.add(msg, level, hl_group)
-            end,
-          })
-
-          vim.api.nvim_create_autocmd({ "User" }, {
-            pattern = "CodeCompanionRequestFinished",
-            group = group,
-            callback = function(ev)
-              local msg, level, hl_group = format_status(ev)
-              local mini_id = ids[ev.data.id]
-              if mini_id then
-                ids[ev.data.id] = nil
-                MiniNotify.update(mini_id, { msg = msg, level = level, hl_group = hl_group })
-              else
-                mini_id = MiniNotify.add(msg, level, hl_group)
-              end
-              vim.defer_fn(function() MiniNotify.remove(mini_id) end, 5000)
-            end,
-          })
-        '';
-      }
-
-      # Cache and restore previous chats
-      codecompanion-history-nvim
 
       # Markview for making codecompanion prettier
       {
