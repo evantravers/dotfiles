5821abf7d64684bc56d9f2b29a407c7a585a82a0
Evan Travers <evantravers@gmail.com>
2026-02-20 17:57:36 -0600

    Fix system error with opencode

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,7 @@
 {
   unstable-packages = final: _prev: {
     unstable = import inputs.nixpkgs-unstable {
-      system = final.system;
+      system = final.stdenv.hostPlatform.system;
       config.allowUnfree = true;
     };
   };
