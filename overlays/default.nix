58ac4e1fd2447535c0b2f39b622fb3b8cdf4d542
Evan Travers <evantravers@gmail.com>
2026-03-05 09:50:51 -0600

    Try devenv main

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,6 +11,7 @@
     llm-agents = inputs.llm-agents.packages.${prev.stdenv.hostPlatform.system};
   };
 
+  devenv = inputs.devenv.overlays.default;
   neovim-nightly = inputs.neovim-nightly-overlay.overlays.default;
   jujutsu = inputs.jujutsu.overlays.default;
 
