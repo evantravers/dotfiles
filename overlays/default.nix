cee3175553293f8eafb6c8486ddc19a4d2915f87
Evan Travers <evantravers@gmail.com>
2026-03-08 14:15:21 -0500

    Remove overlay for inetutils

--- overlays/default.nix
+++ overlays/default.nix
@@ -14,22 +14,4 @@
   devenv = inputs.devenv.overlays.default;
   neovim-nightly = inputs.neovim-nightly-overlay.overlays.default;
   jujutsu = inputs.jujutsu.overlays.default;
-
-  # Workaround for https://github.com/NixOS/nixpkgs/issues/488689
-  # gnulib's error() macro passes dgettext() results as format strings,
-  # triggering -Werror,-Wformat-security in openat-die.c
-  # Solution from https://github.com/wimpysworld/nix-config/commit/4248e93
-  fix-inetutils = _final: prev: {
-    inetutils = prev.inetutils.overrideAttrs (
-      old:
-      prev.lib.optionalAttrs prev.stdenv.isDarwin {
-        env = (old.env or { }) // {
-          NIX_CFLAGS_COMPILE = toString [
-            (old.env.NIX_CFLAGS_COMPILE or "")
-            "-Wno-format-security"
-          ];
-        };
-      }
-    );
-  };
 }
