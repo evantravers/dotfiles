5a64d964f2b468a27b077b12faa3a69fa51cc6e2
Evan Travers <evantravers@gmail.com>
2026-06-18 07:49:27 -0500

    Claude: /simplify mksystem

--- lib/mksystem.nix
+++ lib/mksystem.nix
@@ -15,23 +15,17 @@ name:
 }:
 
 let
-  # True if this is a WSL system.
-  isWSL = wsl;
-
-  # True if Linux, which is a heuristic for not being Darwin.
-  # isLinux = !darwin && !isWSL;
-
   # The config files for this system.
   machineConfig = ../machines/${name}.nix;
   userOSConfig = ../users/${user}/${if darwin then "darwin" else "nixos"}.nix;
   userHMConfig = ../users/${user}/home-manager.nix;
 
-  # NixOS vs nix-darwin functionst
+  # NixOS vs nix-darwin system builders.
   systemFunc = if darwin then inputs.darwin.lib.darwinSystem else nixpkgs.lib.nixosSystem;
   home-manager =
     if darwin then inputs.home-manager.darwinModules else inputs.home-manager.nixosModules;
 in
-systemFunc rec {
+systemFunc {
   inherit system;
 
   modules = [
@@ -44,7 +38,7 @@ systemFunc rec {
     { nixpkgs.config.allowUnfree = true; }
 
     # Bring in WSL if this is a WSL build
-    (if isWSL then inputs.nixos-wsl.nixosModules.wsl else { })
+    (if wsl then inputs.nixos-wsl.nixosModules.wsl else { })
 
     machineConfig
     userOSConfig
@@ -54,18 +48,6 @@ systemFunc rec {
       home-manager.useUserPackages = true;
       home-manager.users.${user} = import userHMConfig;
       home-manager.extraSpecialArgs = { inherit inputs; };
-    }
-
-    # We expose some extra arguments so that our modules can parameterize
-    # better based on these values.
-    {
-      config._module.args = {
-        currentSystem = system;
-        currentSystemName = name;
-        currentSystemUser = user;
-        isWSL = isWSL;
-        inputs = inputs;
-      };
     }
   ];
 }
