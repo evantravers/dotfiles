cac1cea1edaa21f6ea28915b3999eacaf2e9463f
Evan Travers <evantravers@gmail.com>
2026-09-15 14:18:43 -0500

    Add mini status column

--- users/evantravers/nvim.nix
+++ users/evantravers/nvim.nix
@@ -159,13 +159,13 @@
             -- Disabled: the gen_content.main API only exists on mini.nvim's
             -- main branch. Re-enable (along with the vim-plugins-mini-nvim-main
             -- overlay in overlays.nix) once a release > 0.18.0 is tagged.
-            -- require('mini.statuscolumn').setup({
-            --   content = require('mini.statuscolumn').gen_content.main({
-            --     { format = "=lfs", sep = "" }, -- no separator line, just a space
-            --     { ltype = "virt", lnum = "•" }, -- dot in virtual lines
-            --     { ltype = "wrap", lnum = "↳" }, -- arrow in wrapped lines
-            --   }),
-            -- })
+            require('mini.statuscolumn').setup({
+              content = require('mini.statuscolumn').gen_content.main({
+                { format = "=lfs", sep = "" }, -- no separator line, just a space
+                { ltype = "virt", lnum = "•" }, -- dot in virtual lines
+                { ltype = "wrap", lnum = "↳" }, -- arrow in wrapped lines
+              }),
+            })
             require('mini.surround').setup()
             require('mini.splitjoin').setup()
 
